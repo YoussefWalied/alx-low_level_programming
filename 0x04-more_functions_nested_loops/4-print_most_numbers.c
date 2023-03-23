7bdb4ba30f72e5174646d113ed6513bd7a6db76e
YoussefWalied <youssef.walied1@gmail.com>
2023-03-23 16:20:00 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@
 
 void print_most_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = 48; i < 48 + 10; i++)
-    {
-        if (!(i - 48 == 2 || i - 48 == 4))
-            _putchar(i);
-    }
-    _putchar('\n');
+	for (i = 48; i < 48 + 10; i++)
+	{
+		if (!(i - 48 == 2 || i - 48 == 4))
+			_putchar(i);
+	}
+	_putchar('\n');
 }
