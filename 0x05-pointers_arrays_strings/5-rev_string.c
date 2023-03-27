3d1777e03ac25058e4311a1c0b4ad7baaf63c7ca
YoussefWalied <youssef.walied1@gmail.com>
2023-03-27 01:19:10 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+/**
+ * rev_string - reverses a string
+ * @s: the string to be reversed
+ *
+ * Return: void ALWAYS
+ */
+void rev_string(char *s)
+{
+
+	char tmp;
+	int i, len, end;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	end = len - 1;
+
+        for (i = 0; i < len / 2; i++)
+	{
+        	tmp = s[i];
+		s[i] = s[end];
+		s[end--] = tmp;
+	}
+}	
