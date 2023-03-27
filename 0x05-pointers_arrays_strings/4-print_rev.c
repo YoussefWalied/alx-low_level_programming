7a5594081ca370316d719e20e565da7a5b3038a3
YoussefWalied <youssef.walied1@gmail.com>
2023-03-27 01:07:30 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reveerse, followed by a new line.
+ * @s: The string to be printed
+ *
+ * Return: void ALWAYS
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
